8419b5f0621dbb79b264861d989a25864c93496e
ranindu <ranindu.abeyratne@outlook.com>
2025-05-13 14:22:41 +0530

    add constants & update root layout

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Thousand Sunny';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'My first ecommerce store built with NEXT.js';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
