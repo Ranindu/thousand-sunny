5f61879c533827e3aa85d811fa922904a37a57d9
ranindu <ranindu.abeyratne@outlook.com>
2025-05-13 20:01:19 +0530

    add footer, add header logo, cart & login icons

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,41 @@
+import { ShoppingCart, UserIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
+
 const Header = () => {
-    return <>Header</>;
+    return <header className='w-full border-b'>
+        <div className="wrapper flex-between">
+            <div className="flex-start">
+                <Link href="/" className='flex-start'>
+                    <Image
+                        priority={true}
+                        src="/images/logo.jpg"
+                        alt={`${APP_NAME} Logo`}
+                        width={48}
+                        height={48}
+                    />
+                    <span className='hidden lg:block font-bold text-2xl ml-3'>
+                        {APP_NAME}
+                    </span>
+                </Link>
+            </div>
+            <div className='space-x-2'>
+                <Button asChild variant='ghost'>
+                    <Link href="/cart">
+                        <ShoppingCart /> Cart
+                    </Link>
+                </Button>
+                <Button asChild variant='ghost'>
+                    <Link href="/login">
+                        <UserIcon /> Login
+                    </Link>
+                </Button>                
+            </div>
+        </div>
+    </header>
 }
  
 export default Header;
