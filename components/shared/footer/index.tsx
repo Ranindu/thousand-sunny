664905089a660c4faed28afd2ea12fd31fdc8582
ranindu <ranindu.abeyratne@outlook.com>
2025-05-14 08:46:12 +0530

    add footer

--- components/shared/footer/index.tsx
+++ components/shared/footer/index.tsx
@@ -1,5 +1,12 @@
+import { APP_NAME } from '@/lib/constants';
+
 const Footer = () => {
-    return <>Footer</>;
+    const currentYear = new Date().getFullYear(); // Get the current year
+    return (<footer className='border-t'>
+    <div className="p-5 flex-center">
+        {currentYear} {APP_NAME}. All rights reserved.
+    </div>
+</footer>);    
 }
  
 export default Footer;
