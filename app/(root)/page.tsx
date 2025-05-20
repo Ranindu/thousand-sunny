84ec461c3a7af9a8c62c05b840569b67c90e276a
ranindu <ranindu.abeyratne@outlook.com>
2025-05-20 21:56:39 +0530

    add product list and sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,15 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data"
+
 const  Homepage = () => {
-  return <>Thousand Sunny</>;
+  console.log(sampleData)
+  return <>
+    <ProductList 
+    data={sampleData.products} 
+    title='Newest Arrivals'
+    limit={4} 
+    />
+  </>;
 }
  
 export default Homepage;
